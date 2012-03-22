9d0de08e72170f828aab88185230c7484a9aed07
Nicolás Cardelino <ncardeli@gmail.com>
2012-03-22 16:43:55 +0000

    gxui.Message:

--- Controls/Message/MessageRender.js
+++ Controls/Message/MessageRender.js
@@ -1,52 +1,60 @@
-gxui.Message = Ext.extend(gxui.UserControl, function() {
+/// <reference path="..\..\Freezer\Ext\ext-all-dev.js" />
+
+/**
+* @class gxui.Message
+* This control allows you to show a message in two ways: Alert or Notification. 
+* If you want to show an alert, a modal message dialog will appear. 
+* If you want to show a notification, a non-modal notification window will appear, similar to the one used by Microsoft Outlook when a new mail message arrives.
+* 
+*/
+Ext.define('gxui.Message', function () {
 
 	// Private variables
 	var msgCt;
 
 	return {
-		initialize: function() {
-			gxui.Message.superclass.initialize.call(this);
+		extend: 'gxui.UserControl',
 
-			this.Width;
-			this.Height;
-			this.Duration;
-			this.Message;
-			this.Title;
-			this.Icon;
-			this.Cls;
-			this.Show;
+		initialize: function () {
+			this.callParent(arguments);
 		},
 
-		onRender: function() {
+		onRender: function () {
 			if (gxui.CBoolean(this.Show)) {
-				this.ShowMessage(this.Type, this.Title, this.Message);
+				this.ShowMessage();
 			}
 		},
 
-		onRefresh: function() {
+		onRefresh: function () {
 			if (gxui.CBoolean(this.Show)) {
-				this.ShowMessage(this.Type, this.Title, this.Message);
+				this.ShowMessage();
 			}
 		},
 
-		ShowMessage: function(title, message, type) {
-			if (!type) {
-				type = this.Type;
-			}
+		/**
+		* Shows the message.
+		* @param {String} [title] Title of the message. If not specified, the value of {@link #Title} property is used.
+		* @param {String} [message] Text of the message. If not specified, the value of {@link #Message} property is used.
+		* @param {String} [type] Type of the message (see {@link #Type} property). If not specified, the value of {@link #Type} property is used.
+		* @method
+		*/
+		ShowMessage: function (title, message, type) {
+			type = type || this.Type;
+			message = message || this.Message;
+			title = title || this.Title;
 
 			if (type == "alert") {
-				Ext.MessageBox.show({
+				var msgBox = Ext.create('Ext.window.MessageBox');
+				Ext.defer(msgBox.show, 100, msgBox, [{
 					title: title,
 					msg: message,
 					buttons: Ext.MessageBox.OK,
-					/*animEl: '',*/
-					/*fn: showResult,*/
 					icon: this.Icon == "info" ? Ext.MessageBox.INFO : (this.Icon == "question" ? Ext.MessageBox.QUESTION : (this.Icon == "warning" ? Ext.MessageBox.WARNING : Ext.MessageBox.ERROR))
-				});
+				}]);
 			}
 			else {
 				var titleMsgs = (title || "").split("|");
-				Ext.each((message || "").split("|"), function(msg, i) {
+				Ext.each((message || "").split("|"), function (msg, i) {
 					// Create the message box
 					if (!msgCt) {
 						msgCt = Ext.DomHelper.insertFirst(document.body, { id: 'msg-div' }, true);
@@ -57,20 +65,20 @@ gxui.Message = Ext.extend(gxui.UserControl, function() {
 					var m = Ext.DomHelper.append(msgCt, { html: this.createBox(titleMsgs[i], msg) }, true);
 
 					var timeoutId;
-					var hideMessage = function() {
+					var hideMessage = function () {
 						var f;
-						if (this.Position == 'c'){
-							f = function(){
+						if (this.Position == 'c') {
+							f = function () {
 								m.fadeOut({
-								opacity: 0, //can be any value between 0 and 1 (e.g. .5)
-								easing: 'easeOut',
-								remove: true});
+									opacity: 0, //can be any value between 0 and 1 (e.g. .5)
+									easing: 'easeOut',
+									remove: true
+								});
 							};
-						}else
-						{
-							f = function(){
-								m.ghost( this.Position, { remove: true });								
-							}.createDelegate(this);
+						} else {
+							f = Ext.bind(function () {
+								m.ghost(this.Position, { remove: true });
+							}, this);
 						}
 						timeoutId = setTimeout(f, this.Duration * 1000);
 					};
@@ -82,7 +90,7 @@ gxui.Message = Ext.extend(gxui.UserControl, function() {
 					});
 
 					// Do not hide the message box if the mouse is over it
-					m.on('mouseover', function(e) {
+					m.on('mouseover', function (e) {
 						if (timeoutId) {
 							clearTimeout(timeoutId);
 							timeoutId = null;
@@ -90,7 +98,7 @@ gxui.Message = Ext.extend(gxui.UserControl, function() {
 					}, this);
 
 					// If the mouse is outside the message box, schedule its hiding, according to duration
-					m.on('mouseout', function(e) {
+					m.on('mouseout', function (e) {
 						var box = m.getBox();
 						var x = e.getPageX();
 						var y = e.getPageY();
@@ -99,13 +107,13 @@ gxui.Message = Ext.extend(gxui.UserControl, function() {
 								return
 							}
 						}
-						hideMessage.createDelegate(this)();
+						Ext.bind(hideMessage, this)();
 					}, this)
 				}, this);
 			}
 		},
 
-		createBox: function(t, s) {
+		createBox: function (t, s) {
 			return ['<div class="msg">',
 	                '<div class="x-box-tl"><div class="x-box-tr"><div class="x-box-tc"></div></div></div>',
 	                '<div class="x-box-ml"><div class="x-box-mr"><div class="x-box-mc"><h3>', t, '</h3>', s, '</div></div></div>',
--- gxui.jsb
+++ gxui.jsb
@@ -83,13 +83,15 @@
     <include name="ux\GridAutowidth\AutoWidthColumns.js" />
     <include name="Controls\GridExtension\DragDrop.js" />
     <include name="Controls\GridExtension\GridExtension.js" />
-    <include name="Controls\TabPanel\TabPanelRender.js" />
     <include name="Controls\GridExtension\ColumnRenderers.js" />
+    <include name="Controls\TabPanel\TabPanelRender.js" />
+    <include name="Controls\Message\MessageRender.js" />
   </target>
   <target name="gxui-allcss" file="$output\gxui-all.css" debug="False" shorthand="False" shorthand-list="YAHOO.util.Dom.setStyle&#xD;&#xA;YAHOO.util.Dom.getStyle&#xD;&#xA;YAHOO.util.Dom.getRegion&#xD;&#xA;YAHOO.util.Dom.getViewportHeight&#xD;&#xA;YAHOO.util.Dom.getViewportWidth&#xD;&#xA;YAHOO.util.Dom.get&#xD;&#xA;YAHOO.util.Dom.getXY&#xD;&#xA;YAHOO.util.Dom.setXY&#xD;&#xA;YAHOO.util.CustomEvent&#xD;&#xA;YAHOO.util.Event.addListener&#xD;&#xA;YAHOO.util.Event.getEvent&#xD;&#xA;YAHOO.util.Event.getTarget&#xD;&#xA;YAHOO.util.Event.preventDefault&#xD;&#xA;YAHOO.util.Event.stopEvent&#xD;&#xA;YAHOO.util.Event.stopPropagation&#xD;&#xA;YAHOO.util.Event.stopEvent&#xD;&#xA;YAHOO.util.Anim&#xD;&#xA;YAHOO.util.Motion&#xD;&#xA;YAHOO.util.Connect.asyncRequest&#xD;&#xA;YAHOO.util.Connect.setForm&#xD;&#xA;YAHOO.util.Dom&#xD;&#xA;YAHOO.util.Event">
     <include name="resources\css\gxui.css" />
     <include name="Controls\Toolbar\toolbar.css" />
     <include name="ux\grid\CheckHeader.css" />
+    <include name="Controls\Message\message.css" />
   </target>
   <file name="resources\images\iconCollapseAll.gif" path="resources\images" />
   <file name="resources\images\iconExpandAll.gif" path="resources\images" />
@@ -158,4 +160,11 @@
   <file name="Controls\TabPanel\design\TabPanelProperties.xml" path="TabPanel\design" />
   <file name="Controls\TabPanel\TabPanelRender.js" path="TabPanel" />
   <file name="Controls\TabPanel.control" path="" />
+  <file name="Controls\Message\design\MessageRender.xsl" path="Message\design" />
+  <file name="Controls\Message\design\message.ico" path="Message\design" />
+  <file name="Controls\Message\design\MessageProperties.xml" path="Message\design" />
+  <file name="Controls\Message\design\message.png" path="Message\design" />
+  <file name="Controls\Message\MessageRender.js" path="Message" />
+  <file name="Controls\Message\message.css" path="Message" />
+  <file name="Controls\Message.control" path="" />
 </project>
