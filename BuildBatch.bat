275663e4497c3c798a0abf8d5eef6c53d319a298
Nicolás Cardelino <ncardeli@gmail.com>
2012-03-29 15:52:03 +0000

    # Se ajusta el mecanismo de build para armar fuentes Js de GxUI y assemblies .Net.

--- BuildBatch.bat
+++ BuildBatch.bat
@@ -1,4 +1,4 @@
 @echo off
 set Configuration=Debug
 if not .%1.==.. set Configuration=%1
-msbuild.exe /nologo /v:m gxui.msbuild /p:Configuration=%Configuration% /p:Action=Build
+msbuild.exe /nologo /v:m gxui.msbuild /t:BuildAll /p:Configuration=%Configuration% /p:Action=Build
