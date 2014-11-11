63f48681a3e5a71a8b05e7647032d7fa1ff24afa
Nicolás Cardelino <ncardeli@gmail.com>
2014-11-11 13:03:15 +0000

    gxui.GridExtension:
    #Fix: Si el locale del browser era distinto del language de la KB (por ejemplo, browser en Inglés y KB en Español), los separadores decimales y de miles se mostraban mal.

--- Controls/GridExtension/ColumnRenderers.js
+++ Controls/GridExtension/ColumnRenderers.js
@@ -1,282 +1,292 @@
 /// <reference path="..\..\Freezer\Ext\ext-all-dev.js" />
 
-Ext.define('gxui.GridExtension.Column', {
-	extend: 'Ext.grid.column.Column',
-	alias: 'widget.gxui.column',
+Ext.define('gxui.GridExtension.Column', function () {
+	var pictureHelperRegex;
 
-	constructor: function (config) {
-		this.callParent([config]);
-		this.renderer = Ext.bind(this.renderer, this);
-		this.editor = this.defineEditor(this.gxColumn, this.actualColIndex);
-	},
+	return {
+		extend: 'Ext.grid.column.Column',
+		alias: 'widget.gxui.column',
 
-	destroy: function () {
-		delete this.gxGrid;
-		delete this.gxColumn;
-		this.callParent(arguments);
-	},
+		constructor: function (config) {
+			this.callParent([config]);
+			this.renderer = Ext.bind(this.renderer, this);
+			this.editor = this.defineEditor(this.gxColumn, this.actualColIndex);
+		},
 
-	mapDateFormat: function () {
-		switch (gx.dateFormat) {
-			case 'MDY':
-				return "m/d/y";
-			case 'DMY':
-				return "d/m/y";
-			case 'MDY4':
-				return "m/d/Y";
-			case 'DMY4':
-				return "d/m/Y";
-			case 'YMD':
-				return "y/m/d";
-			default:
-				return "Y/m/d";
-		}
-	},
+		destroy: function () {
+			delete this.gxGrid;
+			delete this.gxColumn;
+			this.callParent(arguments);
+		},
 
-	mapTimeFormat: function (gxColumn) {
-		if (gxColumn.gxControl.vStruct) {
-			var nDec = gxColumn.gxControl.vStruct.dec,
-			minutes = nDec > 3,
-			seconds = nDec == 8,
-			hours = nDec > 1;
+		mapDateFormat: function () {
+			switch (gx.dateFormat) {
+				case 'MDY':
+					return "m/d/y";
+				case 'DMY':
+					return "d/m/y";
+				case 'MDY4':
+					return "m/d/Y";
+				case 'DMY4':
+					return "d/m/Y";
+				case 'YMD':
+					return "y/m/d";
+				default:
+					return "Y/m/d";
+			}
+		},
+
+		mapTimeFormat: function (gxColumn) {
+			if (gxColumn.gxControl.vStruct) {
+				var nDec = gxColumn.gxControl.vStruct.dec,
+				minutes = nDec > 3,
+				seconds = nDec == 8,
+				hours = nDec > 1;
+
+				if (gx.timeFormat == 12) {
+					if (hours && minutes && seconds)
+						return "h:i:s A";
+
+					if (hours && minutes)
+						return "h:i A";
+
+					if (hours)
+						return "h A";
+				}
+				else {
+					if (hours && minutes && seconds)
+						return "H:i:s";
+
+					if (hours && minutes)
+						return "H:i";
+
+					if (hours)
+						return "H";
+				}
+			}
 
 			if (gx.timeFormat == 12) {
-				if (hours && minutes && seconds)
-					return "h:i:s A";
-
-				if (hours && minutes)
-					return "h:i A";
-
-				if (hours)
-					return "h A";
+				return "h:i A";
 			}
-			else {
-				if (hours && minutes && seconds)
-					return "H:i:s";
 
-				if (hours && minutes)
-					return "H:i";
+			return "H:i";
+		},
 
-				if (hours)
-					return "H";
-			}
-		}
-
-		if (gx.timeFormat == 12) {
-			return "h:i A";
-		}
-
-		return "H:i";
-	},
-
-	defineEditor: function (gxColumn, actualColIndex) {
-		var types = gx.types;
-		switch (gxColumn.type) {
-			case types.numeric:
-				var colData = this.gxGrid.ParentObject.GXValidFnc[gxColumn.gxId];
-				return {
-					xtype: 'numberfield',
-					allowDecimals: colData.dec > 0 ? true : false,
-					minValue: colData.sign ? Number.NEGATIVE_INFINITY : 0,
-					decimalPrecision: colData.dec,
-					decimalSeparator: gx.decimalPoint,
-					enforceMaxLength: true,
-					maxLength: colData.len,
-					maxValue: Math.pow(10, colData.len - colData.dec - (colData.dec > 0 ? 1 : 0)) - (colData.dec > 0 ? 1 / Math.pow(10, colData.dec) : 0)
-				};
-
-			case types.date:
-				return {
-					xtype: 'datefield',
-					format: this.mapDateFormat()
-				};
-
-			case types.dateTime:
-				return {
-					xtype: 'xdatetime',
-					dateFormat: this.mapDateFormat(),
-					timeFormat: this.mapTimeFormat(gxColumn)
-				};
-
-			default:
-				if (gxColumn.gxControl.type == gx.html.controls.types.multipleLineEdit)
+		defineEditor: function (gxColumn, actualColIndex) {
+			var types = gx.types;
+			switch (gxColumn.type) {
+				case types.numeric:
+					var colData = this.gxGrid.ParentObject.GXValidFnc[gxColumn.gxId];
 					return {
-						xtype: 'textareafield',
-						maxLength: gxColumn.gxControl.maxLength
+						xtype: 'numberfield',
+						allowDecimals: colData.dec > 0 ? true : false,
+						minValue: colData.sign ? Number.NEGATIVE_INFINITY : 0,
+						decimalPrecision: colData.dec,
+						decimalSeparator: gx.decimalPoint,
+						enforceMaxLength: true,
+						maxLength: colData.len,
+						maxValue: Math.pow(10, colData.len - colData.dec - (colData.dec > 0 ? 1 : 0)) - (colData.dec > 0 ? 1 / Math.pow(10, colData.dec) : 0)
 					};
 
-				return {
-					xtype: 'textfield',
-					maxLength: gxColumn.gxControl.maxLength
-				};
-		}
-	},
+				case types.date:
+					return {
+						xtype: 'datefield',
+						format: this.mapDateFormat()
+					};
 
-	formatNumber: function (value, colData) {
-		var picture = colData.pic;
-		var numberFormat = "";
-		var integerPart = "0"
-		if (picture.indexOf(gx.thousandSeparator) >= 0) {
-			integerPart += gx.thousandSeparator + "000";
-		}
-		if (colData.dec > 0)
-			numberFormat = integerPart + gx.decimalPoint + (Ext.util.Format.leftPad("", colData.dec, '0') || '0');
-		else
-			numberFormat = integerPart;
+				case types.dateTime:
+					return {
+						xtype: 'xdatetime',
+						dateFormat: this.mapDateFormat(),
+						timeFormat: this.mapTimeFormat(gxColumn)
+					};
 
-		v = Ext.util.Format.number(value, numberFormat);
+				default:
+					if (gxColumn.gxControl.type == gx.html.controls.types.multipleLineEdit)
+						return {
+							xtype: 'textareafield',
+							maxLength: gxColumn.gxControl.maxLength
+						};
 
-		// Left fill with zeros if applies
-		var matches = picture.match(new RegExp("^[9" + gx.decimalPoint + gx.thousandSeparator + "]+$"));
-		if (matches && matches.length > 0) {
-			v = picture.substr(0, picture.length - v.length).replace(/9/ig, "0") + v;
-		}
-
-		return v + "";
-	},
-
-	mapDatePictureToFormat: function (vStruct) {
-		var dateFormat = function (FormatPart, Picture) {
-			if (FormatPart == 'Y' && Picture.substr(0, 10) == '99/99/9999')
-				return 'Y';
-			else if (FormatPart == 'Y')
-				return 'y';
-			else if (FormatPart == 'M')
-				return 'm';
-			else if (FormatPart == 'D')
-				return 'd';
-			else return '';
-		};
-
-		var dateTimeFormat = function (Dec) {
-			var timeFmt = gx.timeFormat;
-			var DPTF = '', AMPM = '', TimeFmt;
-			if (timeFmt == 12) {
-				DPTF = 'h';
-				AMPM = ' A';
-			} else if (timeFmt == 24) {
-				DPTF = 'H';
-				AMPM = '';
+					return {
+						xtype: 'textfield',
+						maxLength: gxColumn.gxControl.maxLength
+					};
 			}
+		},
 
-			if (Dec == 2)
-				TimeFmt = '';
-			else if (Dec == 5)
-				TimeFmt = ':i';
-			else if (Dec == 8)
-				TimeFmt = ':i:s';
+		formatNumber: function (value, colData) {
+			var extUtilFormat = Ext.util.Format;
+			var picture = colData.pic;
+			var numberFormat = "";
+			var integerPart = "0"
+			if (picture.indexOf(gx.thousandSeparator) >= 0) {
+				integerPart += ",000";
+			}
+			if (colData.dec > 0)
+				numberFormat = integerPart + "." + (extUtilFormat.leftPad("", colData.dec, '0') || '0');
 			else
-				return '';
+				numberFormat = integerPart;
 
-			return DPTF + TimeFmt + AMPM;
-		};
+			extUtilFormat.thousandSeparator = gx.thousandSeparator;
+			extUtilFormat.decimalSeparator = gx.decimalPoint;
+			v = extUtilFormat.number(value, numberFormat);
 
-
-		var Picture = vStruct.dp.pic,
-			Dec = vStruct.dp.dec,
-			Len = vStruct.len,
-			dateFmt = gx.dateFormat,
-			D1 = dateFmt.substr(0, 1),
-			D2 = dateFmt.substr(1, 1),
-			D3 = dateFmt.substr(2, 1),
-			DD1 = dateFormat(D1, Picture),
-			DD2 = dateFormat(D2, Picture),
-			DD3 = dateFormat(D3, Picture),
-			DT = dateTimeFormat(Dec);
-
-		if (Len > 0 && Dec > 0)
-			return DD1 + '/' + DD2 + '/' + DD3 + ' ' + DT;
-		else if (Len > 0)
-			return DD1 + '/' + DD2 + '/' + DD3;
-		else
-			return DT;
-	},
-
-	formatDate: function (value, vStruct) {
-		var gxdate = value.gxdate;
-		if (value - new Date(0, 0, 0, 0, 0, 0, 0) === 0 && gxdate) {
-			var gxFormat = gxdate.SFmt,
-				dp = vStruct.dp;
-			if (dp && dp.pic && dp.pic.indexOf("9999") >= 0) {
-				gxFormat = "Y4";
+			// Left fill with zeros if applies
+			if (!pictureHelperRegex) {
+				pictureHelperRegex = new RegExp("^[9" + gx.decimalPoint + gx.thousandSeparator + "]+$");
 			}
-			return gxdate.emptyDateString(gxFormat);
-		}
-		else {
-			var format = this.mapDatePictureToFormat(vStruct);
-			return Ext.util.Format.date(value, format);
-		}
-	},
-
-	renderer: function (value, metadata, record, rowIndex, colIndex, store) {
-		if (record.isSummary) {
-			return value;
-		}
-
-		var cell = record.raw[this.actualColIndex],
-			col = this.gxColumn,
-			gxControl = cell.column.gxControl,
-			controlTypes = gx.html.controls.types;
-
-		var v = value;
-		if (col.type == gx.types.date || col.type == gx.types.dateTime) {
-			v = this.formatDate(value, gxControl.vStruct);
-		}
-
-		if (col.type == gx.types.numeric && typeof (value) == "number") {
-			v = this.formatNumber(value, this.gxGrid.ParentObject.GXValidFnc[this.gxColumn.gxId]);
-		}
-
-		if (gx.lang.gxBoolean(cell.visible)) {
-			if (!metadata.tdCls) {
-				metadata.tdCls = '';
+			var matches = picture.match(pictureHelperRegex);
+			if (matches && matches.length > 0) {
+				v = picture.substr(0, picture.length - v.length).replace(/9/ig, "0") + v;
 			}
 
-			if (gx.lang.gxBoolean(this.gxGrid.UseThemeClasses) && cell.cssClass) {
-				metadata.tdCls += ' ' + cell.cssClass
+			return v + "";
+		},
+
+		mapDatePictureToFormat: function (vStruct) {
+			var dateFormat = function (FormatPart, Picture) {
+				if (FormatPart == 'Y' && Picture.substr(0, 10) == '99/99/9999')
+					return 'Y';
+				else if (FormatPart == 'Y')
+					return 'y';
+				else if (FormatPart == 'M')
+					return 'm';
+				else if (FormatPart == 'D')
+					return 'd';
+				else return '';
+			};
+
+			var dateTimeFormat = function (Dec) {
+				var timeFmt = gx.timeFormat;
+				var DPTF = '', AMPM = '', TimeFmt;
+				if (timeFmt == 12) {
+					DPTF = 'h';
+					AMPM = ' A';
+				} else if (timeFmt == 24) {
+					DPTF = 'H';
+					AMPM = '';
+				}
+
+				if (Dec == 2)
+					TimeFmt = '';
+				else if (Dec == 5)
+					TimeFmt = ':i';
+				else if (Dec == 8)
+					TimeFmt = ':i:s';
+				else
+					return '';
+
+				return DPTF + TimeFmt + AMPM;
+			};
+
+
+			var Picture = vStruct.dp.pic,
+				Dec = vStruct.dp.dec,
+				Len = vStruct.len,
+				dateFmt = gx.dateFormat,
+				D1 = dateFmt.substr(0, 1),
+				D2 = dateFmt.substr(1, 1),
+				D3 = dateFmt.substr(2, 1),
+				DD1 = dateFormat(D1, Picture),
+				DD2 = dateFormat(D2, Picture),
+				DD3 = dateFormat(D3, Picture),
+				DT = dateTimeFormat(Dec);
+
+			if (Len > 0 && Dec > 0)
+				return DD1 + '/' + DD2 + '/' + DD3 + ' ' + DT;
+			else if (Len > 0)
+				return DD1 + '/' + DD2 + '/' + DD3;
+			else
+				return DT;
+		},
+
+		formatDate: function (value, vStruct) {
+			var gxdate = value.gxdate;
+			if (value - new Date(0, 0, 0, 0, 0, 0, 0) === 0 && gxdate) {
+				var gxFormat = gxdate.SFmt,
+					dp = vStruct.dp;
+				if (dp && dp.pic && dp.pic.indexOf("9999") >= 0) {
+					gxFormat = "Y4";
+				}
+				return gxdate.emptyDateString(gxFormat);
+			}
+			else {
+				var format = this.mapDatePictureToFormat(vStruct);
+				return Ext.util.Format.date(value, format);
+			}
+		},
+
+		renderer: function (value, metadata, record, rowIndex, colIndex, store) {
+			if (record.isSummary) {
+				return value;
 			}
 
-			if (cell.link) {
-				v = Ext.String.format('<a href="{0}" alt="{2}" target="{3}">{1}</a>', cell.link || "", v || "", cell.alt || "", cell.linkTarget || "");
+			var cell = record.raw[this.actualColIndex],
+				col = this.gxColumn,
+				gxControl = cell.column.gxControl,
+				controlTypes = gx.html.controls.types;
+
+			var v = value;
+			if (col.type == gx.types.date || col.type == gx.types.dateTime) {
+				v = this.formatDate(value, gxControl.vStruct);
 			}
 
-			var style = "";
-			if (cell.style)
-				style += this.extractCssProperties(["text-decoration", "color", "background-color", "font-weight"], cell.style);
-
-			// If the cell fires a user event and is enabled, wrap with an anchor tag.
-			if (cell.grid.grid.isCellEventEnabled(cell)) {
-				style += 'cursor:pointer;';
+			if (col.type == gx.types.numeric && typeof (value) == "number") {
+				v = this.formatNumber(value, this.gxGrid.ParentObject.GXValidFnc[this.gxColumn.gxId]);
 			}
 
-			if (style)
-				metadata.style = style;
+			if (gx.lang.gxBoolean(cell.visible)) {
+				if (!metadata.tdCls) {
+					metadata.tdCls = '';
+				}
 
-			//Show Tooltip text if set
-			if (cell.title) {
-				v = Ext.String.format("<span qtip='{0}'>{1}</span>", cell.title, v);
+				if (gx.lang.gxBoolean(this.gxGrid.UseThemeClasses) && cell.cssClass) {
+					metadata.tdCls += ' ' + cell.cssClass
+				}
+
+				if (cell.link) {
+					v = Ext.String.format('<a href="{0}" alt="{2}" target="{3}">{1}</a>', cell.link || "", v || "", cell.alt || "", cell.linkTarget || "");
+				}
+
+				var style = "";
+				if (cell.style)
+					style += this.extractCssProperties(["text-decoration", "color", "background-color", "font-weight"], cell.style);
+
+				// If the cell fires a user event and is enabled, wrap with an anchor tag.
+				if (cell.grid.grid.isCellEventEnabled(cell)) {
+					style += 'cursor:pointer;';
+				}
+
+				if (style)
+					metadata.style = style;
+
+				//Show Tooltip text if set
+				if (cell.title) {
+					v = Ext.String.format("<span qtip='{0}'>{1}</span>", cell.title, v);
+				}
+				return v;
 			}
-			return v;
+			return "";
+		},
+
+		extractCssProperties: function (properties, inputStyle) {
+			var buffer = [];
+			for (var i = 0, len = properties.length; i < len; i++) {
+				var propMatch = inputStyle.match(new RegExp(properties[i] + ":([^;]+);?"));
+				if (propMatch)
+					buffer.push(Ext.String.format(properties[i] + ":{0};", propMatch[1]));
+			}
+			return buffer.join("");
+		},
+
+		getEditorPlugin: function () {
+			var grid = this.ownerCt.ownerCt,
+				pluginId = grid.id + (this.gxGrid.EditModel == 'CellEditModel' ? '-celledit' : '-rowedit');
+			return grid.getPlugin(pluginId);
 		}
-		return "";
-	},
-
-	extractCssProperties: function (properties, inputStyle) {
-		var buffer = [];
-		for (var i = 0, len = properties.length; i < len; i++) {
-			var propMatch = inputStyle.match(new RegExp(properties[i] + ":([^;]+);?"));
-			if (propMatch)
-				buffer.push(Ext.String.format(properties[i] + ":{0};", propMatch[1]));
-		}
-		return buffer.join("");
-	},
-
-	getEditorPlugin: function () {
-		var grid = this.ownerCt.ownerCt,
-			pluginId = grid.id + (this.gxGrid.EditModel == 'CellEditModel' ? '-celledit' : '-rowedit');
-		return grid.getPlugin(pluginId);
-	}
-});
+	};
+}());
 
 Ext.define('gxui.GridExtension.ImageColumn', {
 	extend: 'gxui.GridExtension.Column',
