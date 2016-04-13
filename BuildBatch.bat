53e69a408fb50af3d195f1667946603473885318
Nicolás Cardelino <ncardeli@gmail.com>
2016-04-13 10:48:42 -0300

    Se parametriza número de minor version temporalmente
    Se parametriz número de minor version temporalmente, hasta definir como
    generar uno en Git. Por ahora se aumenta manualmente.

--- BuildBatch.bat
+++ BuildBatch.bat
@@ -1,4 +1,5 @@
 @echo off
 set Configuration=Debug
 if not .%1.==.. set Configuration=%1
-msbuild.exe /nologo /v:m gxui.msbuild /t:BuildAll /p:Configuration=%Configuration% /p:Action=Build
+if not .%2.==.. set Revision=%2
+msbuild.exe /nologo /v:m gxui.msbuild /t:BuildAll /p:Configuration=%Configuration% /p:Revision=%Revision% /p:Action=Build
